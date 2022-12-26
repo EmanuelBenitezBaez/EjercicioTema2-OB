93a26b2a77568046bac749239c4294b958b07859
emanuelb <emanuelbenitezbaez@gmail.com>
2022-12-26 09:49:19 -0300

    1

--- src/PrecioIva/Main.java
+++ src/PrecioIva/Main.java
@@ -9,24 +9,22 @@ Para este ejercicio tendréis que crear una función que reciba un precio y devu
 public class Main {
     public static void main(String[] args) {
         
+ Scanner leerIVA = new Scanner(System.in);
+ double precio,IVA,precioConIVA;
+ System.out.println("Introduce el precio");
+ precio = leerIVA.nextDouble();
 
+        IVA= calcularIVA(precio);
+        precioConIVA = IVA+precio;
+        System.out.println("El precio sin IVA es: "+ precio );
 
+        System.out.println("El precio con IVA es: "+ precioConIVA);
 
-                int iva=21;                                 //valor de iva
-
-                Scanner scanner =new Scanner(System.in);
-                System.out.println("Introduce un precio: ");
-                double variable = scanner.nextDouble();     //precio sin IVA
-                double variable2;                           //precio con IVA
-                variable2 = calcularIva(variable, iva);
-
-                System.out.println("Precio con IVA: "+variable2);
-
-            }
-            private static double calcularIva(double variable, int valor) {
-                double variable1;
-                variable1 = variable + variable*valor/100;
-                return variable1;
+    }
+            public static double calcularIVA(double precio) {
+        double IVA;
+        IVA=precio * 0.21;
+        return IVA;
             }
         }
 
